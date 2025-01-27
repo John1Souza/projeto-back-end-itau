bea2652bb05c090bc0a9d44dcc9a7307b348c887
Johnatas Souza <109979241+John1Souza@users.noreply.github.com>
2025-01-27 16:03:40 -0300

    Add files via upload

---
+++ src/main/java/com/johndev/projetoBackEndItau/ProjetoBackEndItauApplication.java
@@ -0,0 +1,13 @@
+package com.johndev.projetoBackEndItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackEndItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackEndItauApplication.class, args);
+	}
+
+}
